dacb6eb440109dcde0a67e60d727f25432e1fe3f
sreverter <sreverter@uade.edu.ar>
2025-05-21 20:13:47 -0300

    Creado del psedocodigo y primeras funciones
    Pseudocodigo del ejercicio 3 y primeras funciones en codigo, junto con importacion desde distintos archivos

---
+++ funciones.py
@@ -0,0 +1,7 @@
+def prueba_de_funcion():
+    print("No hago nada")
+
+
+def ingresar_nombre_carrera():
+    nombre_carrera = input("Ingrese el nombre de la carrera: ")
+    print("El nombre de la carrera es: ", nombre_carrera)
---
+++ main.py
@@ -0,0 +1,60 @@
+import funciones as prueba
+###
+#ingresar_nombre_carrera()
+#   carrera_nombre = ingrese nombre de la carrera_nombre
+
+prueba.ingresar_nombre_carrera()
+
+#ingresar_cantidad_corredores()
+#   cantidad_corredores = ingrese cantidad de corredores
+
+#class Corredor:
+#   def __init__(self, nombre, edad, tiempo, numero):
+#       mostrar por pantalla (Creando corredor {nombre} con edad {edad} de numero {numero} y tiempo {tiempo})
+#       self.nombre = nombre
+#       self.edad = edad
+#       self.tiempo = tiempo
+#       self.numero = numero
+
+#ingresar_data_corredores()
+#      corredores = []
+#      para i en rango de (cantidad de corredores):
+#          nombre = ingrese nombre del corredor
+#          edad = ingrese edad del corredor
+#          tiempo = ingrese tiempo del corredor
+#          random.randint(1, cantidad de corredores) 
+#          corredores[i] = Corredor(nombre, edad, tiempo, numero)
+#      devolver corredores
+
+#mostrar_ganador()
+#   para i en rango de (cantidad de corredores):
+
+### comparamos los tiempos de los corredores para obtener el menor tiempo y el numero correspondiente del corredor que hizo dicho tiempo. Se va a ver que tipo de busqueda es la mejor para realizar este problema
+
+#       tiempo_ganador = corredores[i].tiempo
+#       numero_ganador = corredores[i].numero
+#       tiempo_a_comparar = corredores[i+1].tiempo
+#       numero_a_comparar = corredores[i+1].numero
+#       si tiempo_ganador es mayor a tiempo_a_comparar:
+#           tiempo_ganador = tiempo a comparar
+#           numero_ganador = numero_a_comparar
+#           incrementar en 1 i
+#       sino
+#           incrementar en 1 i
+#       mostrar tiempo_ganador
+#           
+
+
+#ingresar_tiempo_record()
+#   tiempo_record = ingrese tiempo record
+#   si tiempo_record < tiempo_ganador
+#       mostrar "Se ha roto el tiempo record. El nuevo tiempo record es: {tiempo_record}"
+
+#calcular_promedios_tiempos()
+#   total_tiempo = 0
+#   total_tiempo = total_tiempo + corredores[i].tiempo
+#   promedio_tiempo = total_tiempo / cantidad_corredores
+#   mostrar promedio_tiempo
+
+
+prueba.prueba_de_funcion()
