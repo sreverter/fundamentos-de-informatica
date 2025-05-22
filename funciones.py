17073ea7625f5e8a5a590c05f8a3f2e7112439a0
sreverter <sreverter@uade.edu.ar>
2025-05-22 10:43:48 -0300

    Cambio en el formato de comentado de codigo y agregado de la funcion para ingresar la cantidad de corredores

--- funciones.py
+++ funciones.py
@@ -4,4 +4,10 @@ def prueba_de_funcion():
 
 def ingresar_nombre_carrera():
     nombre_carrera = input("Ingrese el nombre de la carrera: ")
-    print("El nombre de la carrera es: ", nombre_carrera)
+    print("El nombre de la carrera es:", nombre_carrera)
+    return nombre_carrera
+
+def ingresar_cantidad_corredores():
+    cantidad_corredores = int(input("Ingrese la cantidad de corredores: "))
+    print("La cantidad de corredores es:", cantidad_corredores)
+    return cantidad_corredores
