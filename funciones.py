8ee2fb915e8b303579c4742778b9fa4d50a98c50
sreverter <sreverter@uade.edu.ar>
2025-06-05 14:42:05 -0300

    Update
    Agregado de la nueva funcion de ordenamiento de listas apareadas.
    Agregado de nueva funcion para generar los numeros de manera aleatoria y checkeado que no se repitan.
    Implementacion del archivo de funciones para cargar todas las funciones ahi y solo correr el programa en main.
    Limpiado de codigo

--- funciones.py
+++ funciones.py
@@ -1,6 +1,4 @@
-def prueba_de_funcion():
-    print("No hago nada")
-
+import random
 
 def ingresar_nombre_carrera():
     nombre_carrera = input("Ingrese el nombre de la carrera: ")
@@ -13,4 +11,72 @@ def ingresar_cantidad_corredores():
     return cantidad_corredores
 
 
-#Crear funcion de burbujeo con listas apareadas (Santi)
+#funcion que ingresa los datos de los corredores y los guarda en listas apareadas segun su numero, nombre y tiempo
+
+def ingresar_data_corredores():
+    cantidad = ingresar_cantidad_corredores()
+    corredores_nombre = []
+    corredores_numero = []
+    corredores_tiempo = []
+    for i in range(cantidad):
+        corredores_nombre.append(input("Ingrese nombre del corredor: "))
+        #Logica de buscar numeros entre corredores
+        numero_corredor = random.randint(1, cantidad)
+        numero_repetido = busqueda_secuencial(numero_corredor, corredores_numero)
+        if numero_repetido == False:
+            corredores_numero.append(numero_corredor)
+        else:
+            while numero_repetido == True:
+                numero_corredor = random.randint(1, cantidad)
+                numero_repetido = busqueda_secuencial(numero_corredor, corredores_numero)
+            corredores_numero.append(numero_corredor)
+        corredores_tiempo.append(int(input("Ingrese el tiempo del corredor: ")))
+    mostrar_lista_corredores = input("Desea ver la lista de corredores? (si/no): ")
+    if mostrar_lista_corredores.lower() == "si":
+        for i in range(cantidad):
+            print(f"Corredor N°{corredores_numero[i]}: {corredores_nombre[i]} con tiempo {corredores_tiempo[i]}")
+        return corredores_nombre, corredores_tiempo, corredores_numero
+
+#funcion que compara los tiempos de los corredores para obtener el menor tiempo. Al usar listas apareadas, tambien se cambia el nombre y el numero teniendo en cuenta el tiempo del corredor
+def metodoBurbuja(nombre, tiempo, numero):
+    n = len(nombre)
+    for i in range(n-1):
+        cambio_realizado = False
+        for j in range(0, n-i-1):
+            if tiempo[j] > tiempo[j + 1]:
+                tiempo[j], tiempo[j + 1] = tiempo[j + 1], tiempo[j]
+                nombre[j], nombre[j + 1] = nombre[j + 1], nombre[j]
+                numero[j], numero[j + 1] = numero[j + 1], numero[j]
+                cambio_realizado = True
+        if not cambio_realizado:
+            break
+    return nombre[0], tiempo[0], numero[0]  # Retorna el corredor con el menor tiempo
+
+
+#funcion que busca si el numero del corredor ya esta y lo cambia con otro aleatorio
+
+def busqueda_secuencial(valor, numeros):
+    for i in range(len(numeros)):
+        if numeros[i] == valor:
+            return True  # Retorna True si se encuentra el número
+    return False  # Retorna False si no se encuentra el número
+
+
+# Funcion que permite ingresar un tiempo record y compara si es menor al tiempo del ganador
+
+def ingresar_tiempo_record(tiempo_ganador):
+    tiempo_record = int(input("Ingrese el tiempo record: "))
+    if tiempo_record > tiempo_ganador:
+        print(f"Se ha roto el tiempo record. El nuevo tiempo record es: {tiempo_ganador}")
+    else:
+        print(f"No se ha roto el tiempo record. El tiempo record sigue siendo: {tiempo_record} segundos")
+
+# Funcion que calcula el promedio de los tiempos de los corredores y lo muestra por pantalla
+
+def calcular_promedios_tiempos(corredores):
+    total_tiempo = 0
+    n = len(corredores)
+    for i in range(n):
+        total_tiempo += corredores[i]
+    promedio_tiempo = total_tiempo / n
+    print(f"El promedio de tiempo de los corredores es: {promedio_tiempo} segundos")
--- main.py
+++ main.py
@@ -1,151 +1,22 @@
-import funciones as prueba
-#import random
-
-#ingresar_nombre_carrera()
-#    carrera_nombre = ingrese nombre de la carrera_nombre
-# Se ingresa el nombre de la carrera
-
-prueba.ingresar_nombre_carrera()
-
-#ingresar_cantidad_corredores()
-#    cantidad_corredores = ingrese cantidad de corredores
-# Se ingresa la cantidad de corredores que van a participar en la carrera
-
-cantidad = prueba.ingresar_cantidad_corredores() 
-
-'''
-class Corredor:
-    def __init__(self, nombre, edad, tiempo, numero):
-        mostrar por pantalla (Creando corredor {nombre} con edad {edad} de numero {numero} y tiempo {tiempo})
-        self.nombre = nombre
-        self.edad = edad
-        self.tiempo = tiempo
-        self.numero = numero
-'''
-#Se crea el objeto Corredor para guardarlo dentro de un arreglo
-'''
-class Corredor:
-    def __init__(self, nombre, tiempo, numero):
-        print(f"Creando corredor con el nombre {nombre}, el numero {numero}, y el tiempo de {tiempo}")
-        self.nombre = nombre
-        self.tiempo = tiempo
-        self.numero = numero
-        pass
-'''
-
-'''
-ingresar_data_corredores()
-    corredores = []
-    para i en rango de (cantidad de corredores):
-        nombre = ingrese nombre del corredor
-        edad = ingrese edad del corredor
-        tiempo = ingrese tiempo del corredor
-        random.randint(1, cantidad de corredores) 
-        corredores[i] = Corredor(nombre, edad, tiempo, numero)
-    devolver corredores
-'''
-# Funcion en la cual se ingresan los datos de los corredores y se guardan en un arreglo de objetos Corredor
-
-def ingresar_data_corredores():
-    corredores = [] #es lista
-    for i in range(cantidad): #cambiar
-        nombre = input("Ingrese nombre del corredor: ")
-        tiempo = int(input("Ingrese tiempo del corredor: "))
-        #numero = random.randint(1, cantidad)
-        numero = i #duplicado
-        #corredores.append(Corredor(nombre, tiempo, numero))
-        print(corredores)
-    return corredores
-
-'''
-mostrar_ganador()
-    para i en rango de (cantidad de corredores):
-
-### comparamos los tiempos de los corredores para obtener el menor tiempo y el numero correspondiente del corredor que hizo dicho tiempo. Se va a ver que tipo de busqueda es la mejor para realizar este problema
-
-        tiempo_ganador = corredores[i].tiempo
-        numero_ganador = corredores[i].numero
-        tiempo_a_comparar = corredores[i+1].tiempo
-        numero_a_comparar = corredores[i+1].numero
-        si tiempo_ganador es mayor a tiempo_a_comparar:
-            tiempo_ganador = tiempo a comparar
-            numero_ganador = numero_a_comparar
-            incrementar en 1 i
-        sino
-            incrementar en 1 i
-        mostrar tiempo_ganador
-            
-
-'''
-#Funcion que compara los tiempos de los corredores para obtener el menor tiempo y el numero correspondiente del corredor que hizo dicho tiempo. Se utiliza el metodo burbuja para ordenar los tiempos de los corredores.
-
-def metodoBurbuja(corredores):
-    n = len(corredores)
-    for recorrido in range (1, n):
-        for i in range (len(corredores) - recorrido):
-            if corredores[i].tiempo > corredores[i + 1].tiempo:
-                corredores[i], corredores[i + 1] = corredores[i + 1], corredores[i]
-    return corredores[0]  # Retorna el corredor con el menor tiempo
-
-'''
-ingresar_tiempo_record()
-    tiempo_record = ingrese tiempo record
-    si tiempo_record < tiempo_ganador
-        mostrar "Se ha roto el tiempo record. El nuevo tiempo record es: {tiempo_record}"
-'''
-
-# Funcion que permite ingresar un tiempo record y compara si es menor al tiempo del ganador
-
-def ingresar_tiempo_record():
-    tiempo_record = int(input("Ingrese el tiempo record: "))
-    if tiempo_record > tiempo_ganador:
-        print(f"Se ha roto el tiempo record. El nuevo tiempo record es: {tiempo_ganador}")
-    else:
-        print(f"No se ha roto el tiempo record. El tiempo record sigue siendo: {tiempo_record} segundos")
-
-
-'''
-calcular_promedios_tiempos()
-    total_tiempo = 0
-    total_tiempo = total_tiempo + corredores[i].tiempo
-    promedio_tiempo = total_tiempo / cantidad_corredores
-    mostrar promedio_tiempo
-'''
-
-# Funcion que calcula el promedio de los tiempos de los corredores y lo muestra por pantalla
-
-def calcular_promedios_tiempos(corredores):
-    total_tiempo = 0
-    n = len(corredores)
-    for i in range(n):
-        total_tiempo += corredores[i].tiempo
-    promedio_tiempo = total_tiempo / n
-    print(f"El promedio de tiempo de los corredores es: {promedio_tiempo} segundos")
-
-
+import funciones as funcion
 
 #Recorrido normal del programa
 
-lista_corredores = ingresar_data_corredores()
+nombre_carrera = funcion.ingresar_nombre_carrera()
+
+nombres, tiempos, numeros = funcion.ingresar_data_corredores()
 
 checkear_ganador = input("¿Desea ver al ganador? (si/no): ")
 if checkear_ganador == "si":
-    ganador = metodoBurbuja(lista_corredores)
-    print(f"El ganador es {ganador.nombre} con el tiempo de {ganador.tiempo} segundos y el numero de corredor {ganador.numero}")
-    tiempo_ganador = ganador.tiempo
+    ganador = funcion.metodoBurbuja(nombres, tiempos, numeros)
+    print(f"El ganador es {ganador[0]} con el tiempo de {ganador[1]} segundos y el numero de corredor {ganador[2]}")
+    tiempo_ganador = ganador[1]
 ingreso_tiempo_record = input("¿Desea ingresar el tiempo record? (si/no): ")
 if ingreso_tiempo_record == "si":
-    ingresar_tiempo_record()
+    funcion.ingresar_tiempo_record(ganador[1])
 mostrar_promedio = input("¿Desea ver el promedio de tiempos? (si/no): ")
 if mostrar_promedio == "si":
-    calcular_promedios_tiempos(lista_corredores)
+    funcion.calcular_promedios_tiempos(tiempos)
 
 
-
-
-import random
-
-cc = int(input("Cuantos corredores?"))
-for i in range(cc):
-    aleatorio = random.randint(1,99)
-    lista + '_' + aleatorio = []
+# Fin del recorrido normal del programa
